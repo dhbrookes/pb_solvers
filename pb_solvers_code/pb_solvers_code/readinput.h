bbb9f8625822f83b989815f60df43be479288b2e
Lisa Felberg <lfelberg@berkeley.edu>
2016-03-04 19:17:47 -0800

    Adding work from david on an input file and on a setup class

---
+++ pb_solvers_code/pb_solvers_code/readinput.h
@@ -0,0 +1,20 @@
+#ifndef READINP_H
+#define READINP_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cstring>
+//#include "readutil.h"
+#include "setup.h"
+#include "util.h"
+
+
+vector<string> split(string str, char delimiter);
+void findLines(string fline, CSetup & inputRead);
+void findKeyword(vector<string> fline, CSetup & inputRead);
+void readInputFile(string fname, CSetup & readIn);
+
+#endif
+
