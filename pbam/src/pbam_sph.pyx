838d0d810b326c62206fb73a283ab9b7176bd966
Felberg, Lisa <lisa.felberg@pnnl.gov>
2016-06-28 08:27:00 -0700

    Adding strings to sphinx input

--- pbam/src/pbam_sph.pyx
+++ pbam/src/pbam_sph.pyx
@@ -2,15 +2,26 @@ cimport PBAMWrap
 cimport PBAMStruct
 from cython.view cimport array as cvarray
 
+
+cdef extern from "string.h" nogil:
+    size_t strlen   (const char *s)
+    char   *strcpy  (char *pto, const char *pfrom)
+    char   *strncpy (char *pto, const char *pfrom, size_t size)
+
 import logging
 
 _log = logging.getLogger()
 
 cdef class PBAM_Solver:
-  #cdef int _ngiter
   cdef double temp_ # system temperature
   cdef double epsilons_ # solvent dielectric
   cdef double epsiloni_  # molecule dielectric
+  cdef double salt_
+
+  cdef bytes rnameB_
+  cdef bytes rtypeB_
+  cdef char* rname_
+  cdef char* rtype_
 
   cdef int _pid  
 
@@ -18,12 +29,17 @@ cdef class PBAM_Solver:
     self.temp_ = 0
     self.epsilons_ = 0
     self.epsiloni_ = 0
+    self.salt_ = 0
 
-
-  def __init__(self, temp, epsilons, epsiloni):
+  def __init__(self, runname, runtype, temp, epsilons, epsiloni, salt):
+    self.rnameB_ = bytes(runname, 'utf-8')
+    self.rtypeB_ = bytes(runtype, 'utf-8')
+    self.rname_ = self.rnameB_
+    self.rtype_ = self.rtypeB_ 
     self.temp_ = temp
     self.epsilons_ = epsilons
     self.epsiloni_ = epsiloni
+    self.salt_ = salt
 
     import os
     self._pid = os.getpid()
@@ -50,9 +66,22 @@ cdef class PBAM_Solver:
       xyzrc[0, i, 3] = atom['radius']
       xyzrc[0, i, 4] = atom['charge']
 
-    pbamin.temp_ = self.temp_;
-    pbamin.sdiel_ = self.epsilons_;
-    pbamin.idiel_ = self.epsiloni_;
+    # Trying to set variables
+    pbamin.temp_ = self.temp_
+    pbamin.sdiel_ = self.epsilons_
+    pbamin.idiel_ = self.epsiloni_
+    pbamin.salt_ = self.salt_
+
+    print("This is runname", self.rnameB_)
+    print("This is runtype", self.rtypeB_)
+
+    print("This is runname", self.rname_)
+    print("This is runtype", self.rtype_)
+
+    strncpy(pbamin.runType_, self.rtype_, strlen(self.rtype_))
+    print( pbamin.runType_ )
+    strncpy(pbamin.runName_, self.rname_, strlen(self.rname_))
+    print( pbamin.runName_ )
 
     pbamout = PBAMWrap.runPBAMSphinxWrap(
       # trying to figure out next line... seems like a pointer to the xyzr vect
