16d3a8720a812383498f785e4cf1257c1fb4a7fa
Felberg, Lisa <lisa.felberg@pnnl.gov>
2016-07-15 08:26:01 -0700

    Adding some new stuff to the cmake files. PBSAM and PBAM tests are working

--- pbsam/unittest/main.cpp
+++ pbsam/unittest/main.cpp
@@ -37,9 +37,9 @@ using namespace std;
 
 double preclim = 1e-7;    //! precision limit
 const int nvals = 10 ;      //! standard number of poles for testing
-//string test_dir_loc = "../pbsam/pbsam_test_files/gtest/";
+string test_dir_loc = "../pbsam/pbsam_test_files/gtest/";
 //string test_dir_loc = "/Users/felb315/pb_solvers/pbsam/pbsam_test_files/gtest/";
-string test_dir_loc = "/Users/davidbrookes/Projects/pb_solvers/pbsam/pbsam_test_files/gtest/";
+//string test_dir_loc = "/Users/davidbrookes/Projects/pb_solvers/pbsam/pbsam_test_files/gtest/";
 
 //string test_dir_loc = "/Users/lfelberg/Documents/pb_solvers/pbsam/pbsam_test_files/gtest/";
 
