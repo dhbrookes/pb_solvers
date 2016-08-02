fcee45d26ef1db9e7c8c52aec52afa768ee63af1
Lisa Felberg <lfelberg@berkeley.edu>
2016-08-01 18:01:59 -0700

    Maybe have cmake working with LA utilities

--- cmake/modules/FindvecLib.cmake
+++ cmake/modules/FindvecLib.cmake
@@ -11,26 +11,30 @@ if(NOT APPLE)
   return()
 endif()
 
-set(__vlib_incl_suff "Frameworks/vecLib.framework/Versions/Current/Headers")
-set(__accel_incl_suff "Frameworks/Accelerate.framework/Versions/Current/")
+include(FindPackageHandleStandardArgs)
+
+set(__vlib_incl_suff "Frameworks/vecLib.framework/Versions/Current/")
+set(__accel_incl_suff "/System/Library/Frameworks/Accelerate.framework/")
 find_path(vecLib_INCLUDE_DIR clapack.h
-          PATHS /System/Library/${__veclib_include_suffix}
-                /System/Library/${__accel_incl_suff}${__vlib_incl_suff}
+#find_path(vecLib_INCLUDE_DIR libBLAS.dylib
+          PATHS /System/Library/${__vlib_incl_suff}
+                ${__accel_incl_suff}${__vlib_incl_suff}
+                ${CMAKE_OSX_SYSROOT}/${__accel_incl_suff}${__vlib_incl_suff}/Headers/     
          )
 
+MESSAGE( STATUS "name: " ${vecLib_INCLUDE_DIR} )
 
 if (${vecLib_INCLUDE_DIR} MATCHES "vecLib_INCLUDE_DIR-NOTFOUND")
-  set(__vlib_incl_suff "Frameworks/vecLib.framework/Versions/Current/")
-  find_path(vecLib_INCLUDE_DIR libBLAS.dylib
-            PATHS /System/Library/${__vlib_incl_suff}
-                  /System/Library/${__accel_incl_suff}${__vlib_incl_suff}
+  MESSAGE( STATUS "before 2nd find: "  /System/Library/${__accel_incl_suff})
+  find_path(vecLib_INCLUDE_DIR 
+            PATHS /System/Library/${__accel_incl_suff}
            )
 
-  MESSAGE( STATUS "incl 1: " ${vecLib_INCLUDE_DIR} )
+  MESSAGE( STATUS "incl 1: " ${vecLib_INCLUDE_DIR})
 endif()  
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(vecLib DEFAULT_MSG vecLib_INCLUDE_DIR)
+  find_package_handle_standard_args(vecLib DEFAULT_MSG vecLib_INCLUDE_DIR)
+MESSAGE( STATUS "after find call " ${vecLib_INCLUDE_DIR})
 
 if(VECLIB_FOUND)
   if(vecLib_INCLUDE_DIR MATCHES "^/System/Library/Frameworks/vecLib.framework.*")
