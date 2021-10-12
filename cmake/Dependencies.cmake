71a64620ec7868ed2b2e205de6a2fea394c14323
Noah Oblath <noah.oblath@pnnl.gov>
2021-10-11 19:02:19 -0700

    Cleanup commented-out code

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -3,28 +3,6 @@ set(PBSAM_LINKER_LIBS "")
 
 # ---[ BLAS
 
-# if BLA_VENDOR is set
-#  find required
-# else (BLA_VENDOR not set)
-#   find ACML
-#   if found
-#     ...
-#   endif
-#   find MKL
-#   if found
-#     ...
-#   endif
-#   find Apple
-#   if found
-#     ...
-#   endif
-#   find NAS (veclib)
-#   if found
-#     ...
-#   endif
-#   find generic required
-# endif
-
 if(NOT BLA_VENDOR)
   set(BLA_VENDOR "OpenBLAS")
   message(STATUS "BLAS vendor was not set; looking for OpenBLAS")
@@ -48,31 +26,5 @@ endif()
 add_definitions(-D__LAU)
 #include_directories(${???})
 list(APPEND PBSAM_LINKER_LIBS ${BLAS_LIBRARIES})
-
-
-#if(NOT APPLE)
-#  set(BLAS "Open" CACHE STRING "Selected BLAS library")
-#  set_property(CACHE BLAS PROPERTY STRINGS "Atlas;Open;MKL")
-
-#  if(BLAS STREQUAL "Open" OR BLAS STREQUAL "open")
-#    find_package(OpenBLAS)
-#    if( OpenBLAS_FOUND)
-#      include_directories(${OpenBLAS_INCLUDE_DIR})
-#      list(APPEND PBSAM_LINKER_LIBS ${OpenBLAS_LIB})
-#      add_definitions(-D__MKL)
-#      add_definitions(-D__LAU)
-#    endif()
-#  endif()
-#elseif(APPLE)
-#  find_package(vecLib)
-#  if(vecLib_FOUND)
-#    include_directories(${vecLib_INCLUDE_DIR})
-#    MESSAGE( STATUS "Dependencies: " ${vecLib_INCLUDE_DIR} )
-#    list(APPEND PBSAM_LINKER_LIBS ${vecLib_LINKER_LIBS})
-#    add_definitions(-D__MACOS)
-#    add_definitions(-D__LAU)
-#  endif()
-# MESSAGE( STATUS "sys root " ${CMAKE_OSX_SYSROOT} )
-#endif()
 
 MESSAGE( STATUS "linkers: " ${PBSAM_LINKER_LIBS} )
