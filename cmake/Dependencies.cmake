9e945780da9aeda3011d617cc3cbb147d598dc3f
Noah Oblath <noah.oblath@pnnl.gov>
2021-10-14 17:29:52 -0700

    Disable PBSAM for mac for now until BLAS issue is resolved

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,30 +1,32 @@
-# Required for static linking and exported to PBSAM.cmake
-set(PBSAM_LINKER_LIBS "")
+if(ENABLE_PBSAM)
+  # Required for static linking and exported to PBSAM.cmake
+  set(PBSAM_LINKER_LIBS "")
 
-# ---[ BLAS
+  # ---[ BLAS
 
-if(NOT BLA_VENDOR)
-  set(BLA_VENDOR "OpenBLAS")
-  message(STATUS "BLAS vendor was not set; looking for OpenBLAS")
-endif()
+  if(NOT BLA_VENDOR)
+    set(BLA_VENDOR "OpenBLAS")
+    message(STATUS "BLAS vendor was not set; looking for OpenBLAS")
+  endif()
 
-find_package(BLAS REQUIRED)
-message(STATUS "Found requested BLAS: <${BLA_VENDOR}>")
+  find_package(BLAS REQUIRED)
+  message(STATUS "Found requested BLAS: <${BLA_VENDOR}>")
 
-if(BLA_VENDOR MATCHES "^ACML[A-Za-z0-9_]*")
-  add_definitions(-D__ACML)
-elseif(BLA_VENDOR MATCHES "^Intel[A-Za-z0-9_]*")
-  add_definitions(-D__MKL)
-elseif(BLA_VENDOR STREQUAL "Apple")
-  add_definitions(-D__MACOS)
-elseif(BLA_VENDOR STREQUAL "NAS")
-  add_definitions(-D__XCODE)
-elseif(BLA_VENDOR STREQUAL "OpenBLAS")
+  if(BLA_VENDOR MATCHES "^ACML[A-Za-z0-9_]*")
+    add_definitions(-D__ACML)
+  elseif(BLA_VENDOR MATCHES "^Intel[A-Za-z0-9_]*")
+    add_definitions(-D__MKL)
+  elseif(BLA_VENDOR STREQUAL "Apple")
+    add_definitions(-D__MACOS)
+  elseif(BLA_VENDOR STREQUAL "NAS")
+    add_definitions(-D__XCODE)
+  elseif(BLA_VENDOR STREQUAL "OpenBLAS")
 
-endif()
+  endif()
 
-add_definitions(-D__LAU)
-#include_directories(${???})
-list(APPEND PBSAM_LINKER_LIBS ${BLAS_LIBRARIES})
+  add_definitions(-D__LAU)
+  #include_directories(${???})
+  list(APPEND PBSAM_LINKER_LIBS ${BLAS_LIBRARIES})
 
-MESSAGE( STATUS "linkers: " ${PBSAM_LINKER_LIBS} )
+  MESSAGE( STATUS "linkers: " ${PBSAM_LINKER_LIBS} )
+endif(ENABLE_PBSAM)
